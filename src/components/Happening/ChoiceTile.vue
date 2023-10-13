fb84db24ac70dbff6f65165206b0e00da75c3c47
BrandonDM <brandon@experiencedriven.ca>
2023-10-13 07:36:29 -0400

    wi

---
+++ src/components/Happening/ChoiceTile.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-card
+    class="row flex inventoryItem clickable cursor-pointer"
+    style="outline: 2px ridge rgba(50, 0, 0, 0.5)"
+    @click="choiceClicked"
+  >
+    <div class="col text-h5 vertical-center">
+      {{ name }}
+    </div>
+  </q-card>
+</template>
+
+<script lang="ts">
+//import Mod from 'src/models/Mod';
+import Choice from 'src/models/Choice';
+import { PropType, computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {},
+  props: {
+    choice: { type: Object as PropType<Choice>, required: true },
+  },
+  emits: ['choiceClicked'],
+
+  setup(props, context) {
+    function inventoryClicked() {
+      context.emit('inventoryClicked');
+    }
+
+    return {
+      ...props,
+      name: computed(() => props.trait.name),
+      colour: computed(() => (props.active ? 'red' : 'white')),
+
+      //methods
+      inventoryClicked,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.inventoryItem:hover {
+  background-color: rgba(67, 62, 71, 0.25);
+}
+</style>
