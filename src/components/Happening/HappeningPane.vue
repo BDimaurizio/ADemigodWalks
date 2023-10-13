477c1a6e30d29540a124df0f4ca4d1362cb74133
BrandonDM <brandon@experiencedriven.ca>
2023-10-13 07:24:11 -0400

    haening ui

---
+++ src/components/Happening/HappeningPane.vue
@@ -0,0 +1,29 @@
+<template>
+  <h4 style="vertical-align: bottom">{{ happening.title }}</h4>
+  <div>{{ happening.text }}</div>
+  <q-separator />
+  <div class="row justify-center q-mt-xl"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Happening from 'src/models/Happening';
+
+export default defineComponent({
+  components: {},
+  props: {
+    happening: { type: Object as PropType<Happening>, required: true },
+  },
+  emits: [],
+
+  computed: {},
+
+  setup(props) {
+    return {
+      ...props,
+
+      //methods
+    };
+  },
+});
+</script>
